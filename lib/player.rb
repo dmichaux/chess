9ccda8aed6ce46a07b5350e59392ca30c5a15898
David Michaux <michaux.dr@gmail.com>
2017-11-10 08:55:30 -0500

    Add spec and implementation for populating game pieces

--- lib/player.rb
+++ lib/player.rb
@@ -1,7 +1,35 @@
 class Player
+	require_relative 'pieces'
+
+	attr_accessor :pieces
+
 	def initialize
+		@pieces = populate_pieces
 	end
 
 	def take_turn
+	end
+
+	private
+
+	def populate_pieces
+		king = King.new
+		queen = Queen.new
+		bishop1 = Bishop.new
+		bishop2 = Bishop.new
+		knight1 = Knight.new
+		knight2 = Knight.new
+		rook1 = Rook.new
+		rook2 = Rook.new
+		pawn1 = Pawn.new
+		pawn2 = Pawn.new
+		pawn3 = Pawn.new
+		pawn4 = Pawn.new
+		pawn5 = Pawn.new
+		pawn6 = Pawn.new
+		pawn7 = Pawn.new
+		pawn8 = Pawn.new
+		pieces = [king, queen, bishop1, bishop2, knight1, knight2, rook1, rook2,
+			pawn1, pawn2, pawn3, pawn4, pawn5, pawn6, pawn7, pawn8]
 	end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,6 +1,14 @@
 require 'player'
 
 describe 'Player' do
+	
+	before do
+		@player = Player.new
+	end
+
+	it 'has game pieces' do
+		expect(@player.pieces).not_to be_empty
+	end
 
 	describe '.take_turn' do
 	end
